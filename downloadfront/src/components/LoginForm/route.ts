00928289b1a73d84da654b0f974f4a5fa811935b
husongtao <15309487104@163.com>
2024-10-18 09:28:41 +0800

    fix: 新增测试，input为空时不发送请求

---
+++ downloadfront/src/components/LoginForm/route.ts
@@ -0,0 +1,11 @@
+import request from "@/utils/request"
+export default {
+    POST: async () => {
+        // const res = await request({
+        //     url: "localhost:5173",
+        //     method: "POST"
+        // })
+        console.log("");
+
+    }
+}
--- downloadfront/src/components/__tests__/LoginForm.spec.ts
+++ downloadfront/src/components/__tests__/LoginForm.spec.ts
@@ -1,6 +1,7 @@
 import { describe, it, expect, vi } from 'vitest'
 import { mount } from '@vue/test-utils'
 import LoginForm from '@/components/LoginForm' // 调整为你的组件正确路径
+import route from '../LoginForm/route'
 
 describe('LoginForm', () => {
   const wrapper = mount(LoginForm)
@@ -11,18 +12,23 @@ describe('LoginForm', () => {
     expect(wrapper.findAll('el-input').length).toEqual(2)
   })
 
-  it("点击按钮后input框不能为空", async() => {
-    inputs[0].element.textContent = ""
-    inputs[1].element.textContent = ""
+  it("点击按钮后若input框为空不发送网络请求", async() => {
+    const onSubmitSpy = vi.spyOn(wrapper.vm, "submitForm" as never)
+    const POSTSpy = vi.spyOn(route, 'POST');
 
-    const onSubmitSpy = vi.spyOn(wrapper.vm, "onSubmit" as never)
     await button.trigger('click')
-    expect(onSubmitSpy).not.toBeCalled()
+    
+    expect(onSubmitSpy).toHaveBeenCalledOnce()
+    expect(POSTSpy).not.toBeCalled()
   })
 
   it("点击按钮后调用onSubmit函数", async() => {
     // 监听onSubmit函数
-    const onSubmitSpy = vi.spyOn(wrapper.vm, "onSubmit" as never)
+    inputs[0].element.textContent = "username"
+    inputs[1].element.textContent = "password"
+
+    const onSubmitSpy = vi.spyOn(wrapper.vm, "submitForm" as never)
+
     await button.trigger('click')
     expect(onSubmitSpy).toHaveBeenCalledOnce()
   })
